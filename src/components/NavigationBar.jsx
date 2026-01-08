d5c32342c03daf51822b99024adaa6e00d5bd0a6
Yohan Bradley <yohanbradley24@gmail.com>
2026-01-08 11:34:12 +0100

    updated project

---
+++ src/components/NavigationBar.jsx
@@ -0,0 +1,11 @@
+export default function NavigationBar() {
+  return (
+    <nav className="navbar">
+      <h1>GreenLeaf Estates</h1>
+      <ul>
+        <li>Home</li>
+        <li>Properties</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  )
